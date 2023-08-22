42fcf0708c8d00f38905586de517072ac36e99aa
IGabii <128262156+IGabii@users.noreply.github.com>
2023-08-21 22:00:16 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,13 @@
+<?php
+
+$destino = "gabiista8@gmail.com";
+$asunto = "Este Mail fue enviado desde la web";
+$nombre = $_POST["nombre"];
+$email = $_POST["email"];
+$numero = $_POST["numero"];
+$mensaje = $_POST["mensaje"];
+$contenido = "Nombre: " . $nombre . "\nEmail: " . $email . "\nNúmero: " . $numero . "\nMensaje: " . $mensaje;
+$mail = mail($destino,"Nueva Solicitud",$contenido);
+header("Location:exito.html");
+
+?>
